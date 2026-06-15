690ebe830b22f42ff581f184e9e611534c5c8faa
hayden20011017-png <hayden20011017@gmail.com>
2026-06-15 17:12:44 +0800

    app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ CONFIG = {
     'FILE_NAME': 'data.xlsx',
     'DIM_FACTOR': 200,
     'MIN_BILLABLE_WEIGHT': 173,
-    'FUEL_RATE': 0.48,
+    'FUEL_RATE': 0.465,
     'REMOTE_RATE': 28,
     'OVERSIZE_FEE': 50,
 }
